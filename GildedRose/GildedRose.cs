8c9635dec69d994a4a93e5475478d6071fb51fc9
Luigi531 <miniloupiot@gmail.com>
2025-10-20 11:55:24 +0200

    Code Refactored and New Logic Implemented

--- GildedRose/GildedRose.cs
+++ GildedRose/GildedRose.cs
@@ -15,75 +15,72 @@ public class GildedRose
     {
         for (var i = 0; i < Items.Count; i++)
         {
-            if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+            switch (Items[i].Name)
             {
-                if (Items[i].Quality > 0)
-                {
-                    if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                    {
-                        Items[i].Quality = Items[i].Quality - 1;
-                    }
-                }
+                case "Sulfuras, Hand of Ragnaros":
+                    break;
+                case "Aged Brie":
+                    UpdateBrie(Items[i]);
+                    break;
+                case "Backstage passes to a TAFKAL80ETC concert":
+                    UpdatePass(Items[i]);
+                    break;
+                case "Conjured Mana Cake":
+                    UpdateConjured(Items[i]);
+                    break;
+                default:
+                    UpdateBrie(Items[i]);
+                    break;
             }
-            else
+        }
+    }
+    
+    //This method implements the base logic for items handling
+    private void BaseItemUpdate(Item item)
+    {
+        //If the quality of an item was to be negative at the next increment, do not execute the logic
+        if (item.Quality > 0)
+        {
+            //If the Sell Date has passed the quality is reduced by 2, otherwise it's reduce by one at the end of the day
+            item.Quality = item.SellIn < 0 ? item.Quality -= 2 : item.Quality--;
+        }
+    }
+    
+    //This method Updates the Brie Specifically.
+    //The Brie Increments in Quality each day until it reaches fifty
+    private void UpdateBrie(Item item)
+    {
+        if (item.Quality < 50) item.Quality++;
+    }
+    
+    //This method Update the concert passes Specifically
+    //If the quality is not yet maxed out, the method checks the time left before the concert and updates it's quality according to it
+    private void UpdatePass(Item item)
+    {
+        if (item.Quality <= 50)
+        {
+            switch (item.SellIn)
             {
-                if (Items[i].Quality < 50)
-                {
-                    Items[i].Quality = Items[i].Quality + 1;
-
-                    if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (Items[i].SellIn < 11)
-                        {
-                            if (Items[i].Quality < 50)
-                            {
-                                Items[i].Quality = Items[i].Quality + 1;
-                            }
-                        }
-
-                        if (Items[i].SellIn < 6)
-                        {
-                            if (Items[i].Quality < 50)
-                            {
-                                Items[i].Quality = Items[i].Quality + 1;
-                            }
-                        }
-                    }
-                }
+                case var _ when (item.SellIn <= 10 && item.SellIn > 5):
+                    item.Quality += 2;
+                    break;
+                case var _ when (item.SellIn <= 5):
+                    item.Quality += 3;
+                    break;
+                default:
+                    item.Quality++;
+                    break;
             }
-
-            if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-            {
-                Items[i].SellIn = Items[i].SellIn - 1;
-            }
-
-            if (Items[i].SellIn < 0)
-            {
-                if (Items[i].Name != "Aged Brie")
-                {
-                    if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (Items[i].Quality > 0)
-                        {
-                            if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                            {
-                                Items[i].Quality = Items[i].Quality - 1;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        Items[i].Quality = Items[i].Quality - Items[i].Quality;
-                    }
-                }
-                else
-                {
-                    if (Items[i].Quality < 50)
-                    {
-                        Items[i].Quality = Items[i].Quality + 1;
-                    }
-                }
-            }
+        }
+    }
+    
+    //This method handles the conjured items behaviour
+    private void UpdateConjured(Item item)
+    {
+        if (item.Quality > 0)
+        {
+            //If the Sell Date has passed the quality is reduced by 2, otherwise it's reduce by one at the end of the day
+            item.Quality = item.SellIn < 0 ? item.Quality -= 4 : item.Quality -= 2;
         }
     }
 }
---
+++ GildedRose/RefactorTest.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace GildedRoseKata;
+
+public class RefactorTest
+{
+    IList<Item> Items;
+
+    public RefactorTest(IList<Item> Items)
+    {
+        this.Items = Items;
+    }
+
+    
+}
